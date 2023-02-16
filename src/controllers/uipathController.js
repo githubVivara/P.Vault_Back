ca281d38998c3603f924049a9dec2afa609dabe6
FoxHere <vitor.lima777@gmail.com>
2023-02-16 18:51:59 -0300

    Subindo rotas prontas mas com modificação para dev e testes

--- src/controllers/uipathController.js
+++ src/controllers/uipathController.js
@@ -3,34 +3,111 @@ import { api, job } from "../service/api.js";
 import cors from "cors";
 
 class UipathController {
+  //--------------------------------------------------------------------------
   static getSecToken = async (req, res) => {
-    var corsOptions = {
-      origin: "https://cloud.uipath.com",
-    };
-    api
-      .post("identity_/connect/token", {
-        grant_type: "client_credentials",
-        client_id: "1fb25133-63e0-45e0-90b6-2162fad2062e",
-        client_secret: "(rMn(~JtDn(J5dEE",
-        scope: "OR.Folders.Read OR.Jobs  OR.Execution",
-      })
-      .then((resp) => {
-        res.status(200).send(resp.data);
-      })
-      .catch((err) => {
-        res.status(400).send({ "Deu erro": err });
-      });
+    // api
+    //   .post("identity_/connect/token", {
+    //     grant_type: "client_credentials",
+    //     client_id: "1fb25133-63e0-45e0-90b6-2162fad2062e",
+    //     client_secret: "(rMn(~JtDn(J5dEE",
+    //     scope: "OR.Folders.Read OR.Jobs  OR.Execution",
+    //   })
+    //   .then((resp) => {
+    //     res.status(200).send(resp.data);
+    //   });
+
+    res.status(200).send({ token: "token aqui do servidor" });
+    //res.status(500).send({ Erro: "Erro aqui no servidor" });
   };
+  //--------------------------------------------------------------------------
   static startJob = (req, res) => {
     const token = req.headers.authorization;
-    const jobBodyParam = req.body;
-    const jtype = req.query;
+    const reqBody = req.body;
+    const jobBodyParam = {
+      startInfo: {
+        ReleaseKey: "256dc842-073d-4d7a-8794-309bdb16a7fb",
+        Strategy: "ModernJobsCount",
+        JobsCount: 1,
+        RobotIds: [],
+        JobsCount: 1,
+        JobPriority: "Normal",
+        InputArguments: `{'in_branchName':'${reqBody.in_branchName}','in_Request':'${reqBody.in_Request}'}`,
+      },
+    };
+    // job.defaults.headers.authorization = token;
+
+    // job
+    //   .post(
+    //     "/odata/Jobs/UiPath.Server.Configuration.OData.StartJobs",
+    //     jobBodyParam,
+    //     {
+    //       headers: {
+    //         "Content-Type": "application/json",
+    //         "X-UIPATH-OrganizationUnitId": reqBody.OrganizationUnitId,
+    //       },
+    //     }
+    //   )
+    //   .then((resp) => {
+    //     res.status(201).send(resp.data);
+    //   })
+    //   .catch((err) => {
+    //     res.status(500).send(err);
+    //   });
 
     //res.status(200).send("Ok o trabalho pode ser solicitado");
-    res
-      .status(200)
-      .send({ token: token, jobBodyParam: jobBodyParam, jtype: jtype });
+    res.status(200).send({ job: "Trabalho iniciado" });
   };
+  //--------------------------------------------------------------------------
+  static ConsultJobState = (req, res) => {
+    const token = req.headers.authorization;
+    const reqBody = req.body;
+
+    // job.defaults.headers.authorization = token;
+    // job
+    //   .get(`/odata/Jobs?$Filter=Id eq ${reqBody.Id}`, {
+    //     headers: {
+    //       "X-UIPATH-TenantName": "tvivara",
+    //       "X-UIPATH-OrganizationUnitId": reqBody.OrganizationUnitId,
+    //     },
+    //   })
+    //   .then((resp) => {
+    //     res.status(200).send(resp.data);
+    //   })
+    //   .catch((err) => {
+    //     res.status(500).send(err);
+    //   });
+
+    res.status(500).send({ Erro: "Erro no servidor" });
+  };
+  //--------------------------------------------------------------------------
+  static StopJobState = (req, res) => {
+    // const token = req.headers.authorization;
+    // const reqBody = req.body;
+    // const jobBodyParam = {
+    //   jobIds: [reqBody.jobRequestId],
+    //   strategy: "Kill",
+    // };
+    // job.defaults.headers.authorization = token;
+    // job
+    //   .post(
+    //     "/odata/Jobs/UiPath.Server.Configuration.OData.StopJobs",
+    //     jobBodyParam,
+    //     {
+    //       headers: {
+    //         "X-UIPATH-TenantName": "tvivara",
+    //         "X-UIPATH-OrganizationUnitId": reqBody.OrganizationUnitId,
+    //       },
+    //     }
+    //   )
+    //   .then((resp) => {
+    //     res.status(200).send(resp.data);
+    //   })
+    //   .catch((err) => {
+    //     res.status(500).send(err);
+    //   });
+    res.status(500).send({ Erro: "Erro no servidor" });
+  };
+  //--------------------------------------------------------------------------
 }
 
 export default UipathController;
--- src/routes/uipathRoutes.js
+++ src/routes/uipathRoutes.js
@@ -4,5 +4,6 @@ import UipathController from "../controllers/uipathController.js";
 const router = express.Router();
 router.post("/api/", UipathController.getSecToken);
 router.post("/api/job/start/", UipathController.startJob);
-
+router.post("/api/job/consult", UipathController.ConsultJobState);
+router.post("/api/job/stop", UipathController.StopJobState);
 export default router;
